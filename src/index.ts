7eefc17862d6461e14453bd8fabf6e0c6749d2eb
cptvictor95 <cpt.victor@hotmail.com>
2021-03-30 14:38:32 -0300

    repo started with template

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express, { Express } from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+const connection = knex({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+});
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running on port: ${address.port}`);
+  } else {
+    console.error("Failed running the server.")
+  }
+})
+
+export default connection;
